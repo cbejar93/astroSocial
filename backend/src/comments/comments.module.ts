5a2263e628684ec5647f17d4db55429edffbe1db
cbejar93 <35776711+cbejar93@users.noreply.github.com>
2025-07-29 14:38:14 -0700

    feat: add comments with likes

---
+++ backend/src/comments/comments.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { CommentsService } from './comments.service';
+import { CommentsController } from './comments.controller';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [CommentsService],
+  controllers: [CommentsController],
+})
+export class CommentsModule {}
