14fd0b0991fd07bc255e525804ac23f36442a835
cbejar93 <35776711+cbejar93@users.noreply.github.com>
2025-08-12 14:42:25 -0700

    style: update 404 home button styling

--- astrogram/src/pages/NotFoundPage.tsx
+++ astrogram/src/pages/NotFoundPage.tsx
@@ -8,7 +8,7 @@ const NotFoundPage: React.FC = () => {
       <p className="text-gray-400">The page you are looking for does not exist.</p>
       <Link
         to="/"
-        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-500"
+        className="px-4 py-2 bg-purple-600 text-white visited:text-white hover:text-white rounded no-underline hover:bg-purple-500"
       >
         Return Home
       </Link>
