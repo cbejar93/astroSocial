cc4e910ccc2bd5936f524aa45a4e936c52b43888
cbejar93 <35776711+cbejar93@users.noreply.github.com>
2025-12-16 13:26:05 -0500

    Normalize user social links

---
+++ backend/prisma/migrations/20250715174439_add_user_profile_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "bio" VARCHAR(500),
+ADD COLUMN     "facebookUrl" TEXT,
+ADD COLUMN     "instagramUrl" TEXT,
+ADD COLUMN     "tiktokUrl" TEXT,
+ADD COLUMN     "youtubeUrl" TEXT;
